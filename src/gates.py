58abda2de5d2d76e7fc87d8e65521f39108ba541
Juna1013 <jmeron.530@gmail.com>
2026-07-26 22:49:11 +0900

    ファイルの追加

---
+++ src/gates.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+import torch
+
+__all__ = [
+    "N_GATES", "GATE_NAMES", "TRUTH_TABLES",
+    "truth_table", "gate_deps", "basic_terms", "sort_gates", "verilog_expr",
+    "GATE_IDENITIY_A", "GATE_FALES", "GATE_TRUE",
+]
+
+N_GATES = 16
+
